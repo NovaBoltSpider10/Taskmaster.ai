bdc7e398c29bcc5ace0061d8bf629231ddc7a9ef
Shreekar Dittakavi <shreksodyne414@gmail.com>
2025-02-19 21:38:57 -0600

    1

--- server/server.js
+++ server/server.js
@@ -12,4 +12,4 @@ mongoose
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log("MongoDB connection error: ", err));
 
-console.log("hello world");
+console.log("hello world and also whats up");
