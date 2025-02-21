a8d7a1c90c599453989cde0bcbf7de134ec994ee
Shreekar Dittakavi <shreksodyne414@gmail.com>
2025-02-20 19:38:11 -0600

    auth

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { auth } = require('express-openid-connect');
 
+const port = 3000;
 const app = express();
 
 mongoose
-    .connect("mongodb+srv://drive2winjoy:akhrub11@taskmasterai.vnuzv.mongodb.net/?retryWrites=true&w=majority&appName=Taskmasterai",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+    .connect('mongodb+srv://drive2winjoy:akhrub11@taskmasterai.vnuzv.mongodb.net/?retryWrites=true&w=majority&appName=Taskmasterai')
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log("MongoDB connection error: ", err));
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-console.log("hello world and also whats up");
-=======
-console.log("whats up");
->>>>>>> 07d0fb8 (hi)
-=======
-console.log("hello world and also whats up");
->>>>>>> 964b3f1 (1)
+// const config = {
+//     authRequired: false,
+//     auth0Logout: true,
+//     secret: process.env.SECRET,
+//     baseURL: `http://localhost:${port}`,
+//     clientID: process.env.CLIENT_ID,
+//     issuerBaseURL: process.env.ISSUER_URL
+// };
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: 'Ie6l-e0OfY3VyWMnW0Py1xncsNMcnvizs6zWLczypLpdvvb597qZJnQiUSqVXkWh',
+    baseURL: `http://localhost:${port}`,
+    clientID: 'nYgF27Va2mg1dJjoejpaLnA0blsxYVja',
+    issuerBaseURL: 'https://dev-3mwnn06ty4frt075.us.auth0.com'
+};
+
+app.use(auth(config));
+
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
