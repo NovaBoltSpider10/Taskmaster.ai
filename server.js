516c80b2535ec902019232e311274a04fff5e08b
Traman2 <tejassraman@gmail.com>
2025-02-26 02:28:42 -0600

    change usermod path

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const jwtCheck = require('./controllers/jwtCheck.js');
-const User = require('./models/User');
+const User = require('./models/userModel');
 require("dotenv").config();
 
 const port = 3000;
