9a183cf737e8d4b4811c44b631dc5f20962ad16f
Traman2 <tejassraman@gmail.com>
2025-02-26 21:48:09 -0600

    changes to routes

--- express_server/models/taskModel.js
+++ express_server/models/taskModel.js
@@ -7,7 +7,8 @@ const taskSchema = mongoose.Schema({
     resources: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Resource' }],
     status: { type: String, enum: ['pending', 'completed', 'overdue'], default: 'pending' },
     points: Number,
-    textbook: String
+    textbook: String,
+    class: { type: mongoose.Schema.Types.ObjectId, ref: 'Class'}
 });
 
 module.exports = mongoose.model('Task', taskSchema);
