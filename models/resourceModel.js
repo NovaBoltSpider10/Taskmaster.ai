183e7d748a6141ad21196e2951b9023e0a94b796
Traman2 <tejassraman@gmail.com>
2025-02-26 21:50:58 -0600

    changes to model

--- models/resourceModel.js
+++ models/resourceModel.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const calendarSchema = mongoose.Schema({
+const resourceSchema = mongoose.Schema({
     urls: [String],
     websites: [String],
     class: { type: mongoose.Schema.Types.ObjectId, ref: 'Class' }
 });
 
-module.exports = mongoose.model('Calendar', calendarSchema);
+module.exports = mongoose.model('Calendar', resourceSchema);
