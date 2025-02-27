40d85d98c074e75b4268ba31fd2b4c249c59e6a9
Traman2 <tejassraman@gmail.com>
2025-02-26 21:10:15 -0600

    change req fields

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true }, //Rq
+    firstName: { type: String},
+    lastName: String,
+    password: { type: String, required: true }, //Rq
+    email: { type: String, required: true, unique: true }, //Rq
     pfp: String,
 
     phoneNumber: String,
