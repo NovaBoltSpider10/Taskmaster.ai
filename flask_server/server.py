d5de18a4106e394cc70bbc9b82a1bbf327d988ab
sriyuthy <sriyuth.y@gmail.com>
2025-03-30 20:05:38 -0500

    sdfdf

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,11 +1,25 @@
 from flask import Flask
+import numpy as np
 
 app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
 def get_data():
-    return "Test data!"
+    return "Hello! <br> Test!"
+
+a = np.array([1, 2, 3])
+b = np.array([2, 3, 4])
+c = a + b
+print (c)
+
+
+
+
+userProfiles = [{"user1": "bob", "classes": ["Computer Science 2," "Linear Algebra," "Differential Calculus"]},
+                {"user2": "mark", "classes": ["Computer Science 2", "World History", "English"]},
+                {"user3": "gabe", "classes": ["Linear Algebra", "Differential Calculus", "Machine Learning"]}]
+
 
 if __name__ == '__main__':
     app.run(host="localhost", port="5000")
