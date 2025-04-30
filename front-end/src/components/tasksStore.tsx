1618f8d7624f6ff5faca44b14ed4c532aa1b0a54
Shreekar <shreksodyne414@gmail.com>
2025-04-30 16:25:18 -0500

    ok

--- front-end/src/components/tasksStore.tsx
+++
@@ -1,9 +0,0 @@
-import type { TasksData } from "../pages/Tasks";
-
-let tasksCache: TasksData[] = [];
-
-export const getTasks = () => tasksCache;
-
-export const setTasks = (tasks: TasksData[]) => {
-  tasksCache = tasks;
-};
