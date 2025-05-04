c569876861e39457faceee32456e2fb877e625d8
Shreekar <shreksodyne414@gmail.com>
2025-05-03 22:04:08 -0500

    restructuring

--- front-end/src/pages/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import './Dashboard.css';
-
-function Dashboard() {
-  return (
-    <h1 className="text-gray-800 text-2xl font-semibold">This is the dashboard</h1>
-  );
-}
-
-export default Dashboard;
